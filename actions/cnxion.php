34304f64a316d535fdfe130f492e8642f3787162
loukou jess <loukoujess85@gmail.com>
2022-08-25 16:00:57 +0100

    first1 commit

--- actions/cnxion.php
+++ actions/cnxion.php
@@ -1,7 +1,7 @@
 <?php
     try {
         session_start();
-        $cnx = new pdo ('mysql:host=localhost;dbname=espace_adminer;charset=utf8','root','');
+        $cnx = new pdo ('mysql:host=localhost;dbname=espace_adminer;charset=utf8','root','bostam');
     } catch (Exception $e) {
         die ("une erreure a été trouvé :". $e->getMessage());
     }
